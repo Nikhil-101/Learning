3b9560eba504f957c1b15cc1cff6b8150110caf0
Nikhil Dhiman <nikhildhiman101@gmail.com>
2024-03-20 18:28:37 +0530

    JS: Creating an Element

--- Web Development/sigma_web_dev/3.JavaScript/0.exercise/0.dom/script.js
+++ Web Development/sigma_web_dev/3.JavaScript/0.exercise/0.dom/script.js
@@ -7,3 +7,8 @@ bodyArrow.firstElementChild.children[0].style.backgroundColor="grey";
 document.querySelectorAll(".box").forEach(e=>{
   e.style.backgroundColor="grey";
 })
+
+// Create an element
+let div = document.createElement("div");
+div.innerHTML="<h2>Heading 2</h2>";
+document.body.appendChild(div);
