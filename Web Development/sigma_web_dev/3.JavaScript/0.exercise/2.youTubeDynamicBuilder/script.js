7fc1152a0cf485050f48b81ed76a31dc2786db99
Nikhil Dhiman <nikhildhiman101@gmail.com>
2024-03-21 13:19:15 +0530

    JS Exercise: Dynamic Website | Formatting Done

--- Web Development/sigma_web_dev/3.JavaScript/0.exercise/2.youTubeDynamicBuilder/script.js
+++ Web Development/sigma_web_dev/3.JavaScript/0.exercise/2.youTubeDynamicBuilder/script.js
@@ -8,62 +8,47 @@ let titleBody = document.createElement('div');
 let title = document.createElement('h3');
 let para = document.createElement('p');
 
-container.appendChild(thumbnail);
-container.append(titleBody);
+// container.appendChild(thumbnail);
+container.insertAdjacentElement('afterbegin', thumbnail);
+// container.append(titleBody);
+container.insertAdjacentElement('beforeend', titleBody);
 thumbnail.appendChild(time);
 
-container.style.display='flex';
-container.style.justifyContent='center';
+container.style.display = 'flex';
+container.style.justifyContent = 'center';
 
 thumbnail.setAttribute('class', 'thumbnail');
-thumbnail.style.width='18rem';
-thumbnail.style.height='10rem';
-thumbnail.style.position='relative';
-thumbnail.style.display='inline-block';
-thumbnail.style.backgroundImage='url("resources/hqdefault.webp")';
-thumbnail.style.backgroundRepeat='no-repeat';
-thumbnail.style.backgroundSize='contain';
-thumbnail.style.borderRadius='.7em';
+thumbnail.style.width = '18rem';
+thumbnail.style.height = '10rem';
+thumbnail.style.position = 'relative';
+thumbnail.style.display = 'inline-block';
+thumbnail.style.backgroundImage = 'url("resources/hqdefault.webp")';
+thumbnail.style.backgroundRepeat = 'no-repeat';
+thumbnail.style.backgroundSize = 'contain';
+thumbnail.style.borderRadius = '.7em';
+thumbnail.style.marginRight = '.7em'
 
 time.setAttribute('class', 'videoTime');
-time.innerText='31:20'
-time.style.backgroundColor='#111';
-time.style.color='white';
-time.style.display='inline';
-time.style.paddingTop='.1em';
-time.style.paddingBottom='.2em';
-time.style.borderRadius='.3em';
-time.style.position='absolute';
-time.style.right='.6em';
-time.style.bottom='.5em';
+time.innerText = '31:20'
+time.style.backgroundColor = '#111';
+time.style.color = 'white';
+time.style.display = 'inline';
+time.style.paddingTop = '.1em';
+time.style.paddingBottom = '.2em';
+time.style.paddingLeft = '.2em';
+time.style.paddingRight = '.2em';
+time.style.borderRadius = '.3em';
+time.style.position = 'absolute';
+time.style.right = '.6em';
+time.style.bottom = '.5em';
 
 titleBody.setAttribute('class', 'titleBody');
-titleBody.style.display='inline-block';
-titleBody.style.marginLeft='.4em';
+titleBody.style.display = 'inline-block';
 
 title.setAttribute('class', 'title');
-title.style.marginTop='.2em';
-title.style.marginBottom='0';
+document.querySelector('.title').style.marginTop = '.4em';
+// title.style.margin = '0em';
 
 para.setAttribute('class', 'metaData');
-para.style.marginTop='.7em';
-para.style.marginBottom='0';
-// thumbnail.style.='';
-// let imgThumbnail = document.createElement('img');
-// let imgTime = document.createElement('div');
-
-// imgThumbnail.setAttribute('src', 'https://i.ytimg.com/vi/KtL-SQ20Q0s/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLABJhdIBqlilwXKzJqa6StynbJT4Q');
-// imgThumbnail.style.position='relative';
-// imgThumbnail.setAttribute('class', 'thumbnail')
-// document.querySelector('.container').appendChild(imgThumbnail);
-
-// document.querySelector('.thumbnail').appendChild(imgTime);
-// imgTime.style.width='1.5rem';
-// imgTime.style.height='.5rem';
-// imgTime.style.backgroundColor='#222';
-// imgTime.style.color='white';
-// imgTime.style.zIndex='2';
-// imgTime.style.position='absolute';
-// imgTime.style.bottom='10px';
-// imgTime.style.right='10px';
-// imgTime.innerText='31:20';
+para.style.marginTop = '.7em';
+para.style.marginBottom = '0';
