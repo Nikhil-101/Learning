fe5aeff1e2f3b52eaab8837279d9e0b22ae2ce84
Nikhil Dhiman <nikhildhiman101@gmail.com>
2025-01-28 17:08:01 +0000

    JS Final Proj; Add Product Cards Using JavaScript

--- Projects/WebDev/8.EcomFinalJS/finalJSProject/src/main.js
+++ Projects/WebDev/8.EcomFinalJS/finalJSProject/src/main.js
@@ -1,3 +1,41 @@
 import './style.css';
+import products from "../api/products.json";
 
-console.log('Hello World!!!')
+console.log('Started')
+
+
+const productListRef = document.querySelector('.selling-product-list');
+const templateRef = document.getElementById("template-product");
+
+products.forEach((product)=>{
+  const clone = templateRef.content.cloneNode(true);
+  const productCategoryRef = clone.querySelector('.product-category');
+  const productImgRef = clone.querySelector('.product-img');
+  const productNameRef = clone.querySelector('.product-name');
+  const productDescriptionRef = clone.querySelector('.product-description');
+  const productPriceRef = clone.querySelector('.product-price');
+  const actualPriceRef = clone.querySelector('.actual-price');
+  const stocksNumRef = clone.querySelector('.stocks-num');
+  // Product increment and decrement btns
+  const addCartBtnRef = clone.querySelector('.add-cart-btn');
+
+  productCategoryRef.innerText = product.category;
+  productImgRef.src = product.image;
+  productNameRef.innerText = product.name;
+  productDescriptionRef.innerText = product.description;
+  productPriceRef.innerText = `₹${product.price}`;
+  actualPriceRef.innerText = `₹${(Number(product.price)*3).toFixed(2)}`;
+  stocksNumRef.innerText = product.stock;
+
+  productListRef.appendChild(clone);
+})
+
+
+
+
+
+console.log(products);
+console.log('Ended');
+
+
+// let clone = 
