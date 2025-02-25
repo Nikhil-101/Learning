9345d3564def520208db469425db56e06c9cd004
Nikhil Dhiman <nikhildhiman101@gmail.com>
2025-02-25 07:36:56 +0000

    React Router: Started

---
+++ Web Development/thapa_technical/reactJS/react_router/components/App.jsx
@@ -0,0 +1,34 @@
+import { About } from "./About"
+import { Contact } from "./Contact"
+import { Home } from "./Home"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AppLayout } from "./layout/AppLayout"
+
+
+
+export const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <AppLayout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/about',
+          element: <About  />,
+        },
+        {
+          path: '/contact',
+          element: <Contact />,
+        }
+      ]
+    },
+    
+  ])
+
+  return <RouterProvider router={router} />
+}
