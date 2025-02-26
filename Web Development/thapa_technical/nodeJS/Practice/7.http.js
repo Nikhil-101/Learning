569e400fc0af1efca698036a0237c742eedc799e
Nikhil Dhiman <nikhildhiman101@gmail.com>
2025-02-26 07:58:38 +0000

    NodeJS Installed nodemon

--- Web Development/thapa_technical/nodeJS/Practice/7.http.js
+++ Web Development/thapa_technical/nodeJS/Practice/7.http.js
@@ -2,11 +2,15 @@ const http = require('http');
 
 const PORT = '3000'
 
+// Server is like an EventEmitter
 const server = http.createServer((req, res)=>{
+  // creating routes
   if(req.url === '/'){
     // To show what type of data is send (html or plain or others)
     res.setHeader('Content-Type', 'text/html')
+    // store data to send in writable
     res.write('<h1>Hello World!!!</h1><br /><a href="localhost:3000/contact">Contact</a>');
+    // send the data
     res.end();
   }
   if(req.url === '/contact'){
@@ -16,6 +20,7 @@ const server = http.createServer((req, res)=>{
   }
 })
 
+// like an emitter.on
 server.listen(PORT, ()=>{
   console.log(`Server is listening at ${PORT}`);
 })
