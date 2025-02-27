049a3ee31a140404dacc355d9c900bdf69fa2fd4
Nikhil Dhiman <nikhildhiman101@gmail.com>
2025-02-27 05:05:55 +0000

    NodeJS: todo App Mini Project

--- Web Development/thapa_technical/nodeJS/Practice/miniProject/1.website1/server.mjs
+++ Web Development/thapa_technical/nodeJS/Practice/miniProject/1.website1/server.mjs
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import http from 'http';
 
 const PORT = '3000';
@@ -19,6 +19,32 @@ const server = http.createServer((req, res)=>{
     if (req.url === '/style.css') {
       sendFile('./style.css', 'text/css');
     }
+  }
+
+  if (req.method === 'POST' && req.url === '/add') {
+    let formData;
+    req.on('data', (data)=>{
+      formData=data;
+    })
+    req.on('end', async ()=>{
+      console.log(formData.toString());
+      const { formHeading, formBody } = JSON.parse(formData.toString());
+      let prevData = await readFile('storage.json', 'utf-8');
+      prevData = JSON.parse(prevData);
+      console.log(formHeading, formBody);
+      console.log(prevData);
+      if (prevData[formHeading]) {
+        console.log('Entry Already Exists');
+        res.writeHead(400, {'Content-Type': 'text/pain'});
+        return res.end('Entry Already Exists')
+      }
+      prevData[formHeading] = formBody;
+      prevData = JSON.stringify(prevData)
+      await writeFile('storage.json', prevData);
+      
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({ success: true , formHeading: formBody}));
+    })
   }
 })
 
